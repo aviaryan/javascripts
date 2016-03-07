2b4c63f0fadf4828cfdd4b07eda3704b7c86459d
aviaryan <avi.aryan123@gmail.com>
2016-03-07 11:14:29 +0530

    improved printing, beautified buttons

---
+++ github.md/script.js
@@ -0,0 +1,33 @@
+
+/**
+ * Calls the github API and refreshes the markdown preview
+ */
+function refreshPreview() {
+    var api_url = 'https://api.github.com/markdown';
+    var printElem = printElem;
+    $.ajax({
+        type: "POST",
+        url: api_url,
+        data: JSON.stringify({
+            "text": $("#input-markdown").get(0).value,
+            "mode": "gfm",
+            "context": $("#repo-name").get(0).value
+        }),
+        async: true,
+        process_data: false,
+        beforeSend: function (jqXHR, PlainObject) {
+            $("#btnSubmit").get(0).textContent = 'Loading..';
+        },
+        error: function (jqXHR, textStatus, error) {
+            console.log(jqXHR, textStatus, error);
+        },
+        success: function (data) {
+            console.log("success!");
+            console.log(data);
+            $("#output-html").get(0).innerHTML = data;
+        },
+        complete: function (jqXHR, textStatus) {
+            $("#btnSubmit").get(0).textContent = 'Refresh';
+        }
+    });
+}
